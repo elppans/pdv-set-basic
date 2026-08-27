2aa2668a950135ef40930f484bbeaac3d1ea5595
elppansmk <elppansmk@gmail.com>
2026-08-27 11:03:37 -0300

    Atualizado comandos

--- arquivos/bin/pdv-info.sh
+++ arquivos/bin/pdv-info.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cd /Zanthus/Zeus/pdvJava || exit 1
+cmd="$0"
+basecmd="$(readlink -m "$cmd")"
+dircmd="$(dirname "$basecmd")"
+LOCALSH="$dircmd"
+PDVPATH="/Zanthus/Zeus/pdvJava"
+PATH="$LOCALSH:$PATH"
+export PDVPATH
+export PATH
+
+cd "$PDVPATH" || exit 1
 #pwd
 #chmod +x xmlstarlet
 #./xmlstarlet --version 2>/dev/null
@@ -15,14 +24,25 @@ cd /Zanthus/Zeus/pdvJava || exit 1
 
 #ls INFOPDV*
 echo __________
-echo "Loja: $(./xmlstarlet sel -t -v "//CODLOJA" -n ./INFOPDV.XML )" 2>/dev/null
-echo "ECF: $(./xmlstarlet sel -t -v "//NUMEROPDV" -n ./INFOPDV.XML )" 2>/dev/null
-echo "IP: $(./xmlstarlet sel -t -v "//IP" -n ./INFOPDV.XML )" 2>/dev/null
-echo "SO: $(./xmlstarlet sel -t -v "//DISTRO_SO" -n ./INFOPDV.XML )" 2>/dev/null
+echo "Loja: $(xmlstarlet sel -t -v "//CODLOJA" -n ./INFOPDV.XML )" 2>/dev/null
+echo "ECF: $(xmlstarlet sel -t -v "//NUMEROPDV" -n ./INFOPDV.XML )" 2>/dev/null
+echo "IP: $(xmlstarlet sel -t -v "//IP" -n ./INFOPDV.XML )" 2>/dev/null
+echo "SO: $(xmlstarlet sel -t -v "//DISTRO_SO" -n ./INFOPDV.XML )" 2>/dev/null
 echo __________
 if [ -f /etc/canoalinux-release ];then
 echo "CANOALINUX:"
 cat /etc/canoalinux-release
+else
+	echo "CANOALINUX: (Arquivo não existe)"
+	echo "PDV V. 12.04 R. 2X"
+fi
+echo __________
+echo "Dispositivos de armazenamento (0 = SSD/NVME, 1 = HDD, loop = Dispositivo de Bloco Virtual)"
+if ! lsblk -d -o name,rota 2>/dev/null ;then
+echo -e "NAME   ROTA" &&
+for d in /sys/block/*; do
+    [ -f "$d/queue/rotational" ] && echo "$(basename "$d"): $(cat "$d/queue/rotational")"
+done
 fi
 echo __________
 lscpu | grep -i -E 'Model:|Modelo:'
