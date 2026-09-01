be4b015ef7f676a290c001492363edba6e97ce77
elppansmk <elppansmk@gmail.com>
2026-09-01 17:10:01 -0300

    Atualizado script para descanso gif

--- arquivos/bin/descanso_path_comum_gif.sh
+++ arquivos/bin/descanso_path_comum_gif.sh
@@ -2,20 +2,26 @@
 
 data="$(date +%Y%m%d_%H%M)"
 
+image1="Wallpaper_Lopes_800x600.gif"
+image2="Wallpaper+Lopes+800x600.gif"
+
+mv -v "/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image1" \
+"/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image2"
+
 if [ -d /Zanthus/Zeus/path_comum_servidor ]; then
   if mount | grep -q path_comum_servidor ; then
     if [ -d /Zanthus/Zeus/path_comum_servidor/Descanso ];then
 	rsync -ah /Zanthus/Zeus/path_comum_servidor/Descanso /Zanthus/Zeus/backup_"$data"/ ;
-	cp -rf /Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/saida_animada.gif /Zanthus/Zeus/path_comum_servidor/Descanso/imagens/
+	cp -rf "/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image2" /Zanthus/Zeus/path_comum_servidor/Descanso/imagens/
     fi
   fi
 fi
 
 if [ -d /Zanthus/Zeus/path_comum ]; then
-  if mount | grep -q path_comum ; then
+  if ! mount | grep -q path_comum ; then
     if [ -d /Zanthus/Zeus/path_comum/Descanso ];then
 	rsync -ah /Zanthus/Zeus/path_comum/Descanso /Zanthus/Zeus/backup_"$data"/ ;
-	cp -rf /Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/saida_animada.gif /Zanthus/Zeus/path_comum/Descanso/imagens/
+	cp -rfv "/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image2" /Zanthus/Zeus/path_comum/Descanso/imagens/
     fi
   fi
 fi
