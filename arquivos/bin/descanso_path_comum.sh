de61f1af2825b4988e31343e90865f8497646d81
elppansmk <elppansmk@gmail.com>
2026-09-01 17:15:23 -0300

    Formatado arquivos

--- arquivos/bin/descanso_path_comum.sh
+++ arquivos/bin/descanso_path_comum.sh
@@ -3,24 +3,21 @@
 data="$(date +%Y%m%d_%H%M)"
 
 if [ -d /Zanthus/Zeus/path_comum_servidor ]; then
-  if mount | grep -q path_comum_servidor ; then
-    if [ -d /Zanthus/Zeus/path_comum_servidor/Descanso ];then
-	rsync -ah /Zanthus/Zeus/path_comum_servidor/Descanso /Zanthus/Zeus/backup_"$data"/ ;
-	rm -rf /Zanthus/Zeus/path_comum_servidor/Descanso
-    fi
-	cp -rf /Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj /Zanthus/Zeus/path_comum_servidor/Descanso
-  fi
+	if mount | grep -q path_comum_servidor; then
+		if [ -d /Zanthus/Zeus/path_comum_servidor/Descanso ]; then
+			rsync -ah /Zanthus/Zeus/path_comum_servidor/Descanso /Zanthus/Zeus/backup_"$data"/
+			rm -rf /Zanthus/Zeus/path_comum_servidor/Descanso
+		fi
+		cp -rf /Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj /Zanthus/Zeus/path_comum_servidor/Descanso
+	fi
 fi
 
 if [ -d /Zanthus/Zeus/path_comum ]; then
-  if mount | grep -q path_comum ; then
-    if [ -d /Zanthus/Zeus/path_comum/Descanso ];then
-	rsync -ah /Zanthus/Zeus/path_comum/Descanso /Zanthus/Zeus/backup_"$data"/ ;
-	rm -rf /Zanthus/Zeus/path_comum/Descanso
-    fi
-	cp -rf /Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj /Zanthus/Zeus/path_comum/Descanso
-  fi
+	if mount | grep -q path_comum; then
+		if [ -d /Zanthus/Zeus/path_comum/Descanso ]; then
+			rsync -ah /Zanthus/Zeus/path_comum/Descanso /Zanthus/Zeus/backup_"$data"/
+			rm -rf /Zanthus/Zeus/path_comum/Descanso
+		fi
+		cp -rf /Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj /Zanthus/Zeus/path_comum/Descanso
+	fi
 fi
-
-
-
--- arquivos/bin/descanso_path_comum_gif.sh
+++ arquivos/bin/descanso_path_comum_gif.sh
@@ -6,25 +6,22 @@ image1="Wallpaper_Lopes_800x600.gif"
 image2="Wallpaper+Lopes+800x600.gif"
 
 mv -v "/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image1" \
-"/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image2"
+	"/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image2"
 
 if [ -d /Zanthus/Zeus/path_comum_servidor ]; then
-  if mount | grep -q path_comum_servidor ; then
-    if [ -d /Zanthus/Zeus/path_comum_servidor/Descanso ];then
-	rsync -ah /Zanthus/Zeus/path_comum_servidor/Descanso /Zanthus/Zeus/backup_"$data"/ ;
-	cp -rf "/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image2" /Zanthus/Zeus/path_comum_servidor/Descanso/imagens/
-    fi
-  fi
+	if mount | grep -q path_comum_servidor; then
+		if [ -d /Zanthus/Zeus/path_comum_servidor/Descanso ]; then
+			rsync -ah /Zanthus/Zeus/path_comum_servidor/Descanso /Zanthus/Zeus/backup_"$data"/
+			cp -rf "/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image2" /Zanthus/Zeus/path_comum_servidor/Descanso/imagens/
+		fi
+	fi
 fi
 
 if [ -d /Zanthus/Zeus/path_comum ]; then
-  if ! mount | grep -q path_comum ; then
-    if [ -d /Zanthus/Zeus/path_comum/Descanso ];then
-	rsync -ah /Zanthus/Zeus/path_comum/Descanso /Zanthus/Zeus/backup_"$data"/ ;
-	cp -rfv "/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image2" /Zanthus/Zeus/path_comum/Descanso/imagens/
-    fi
-  fi
+	if ! mount | grep -q path_comum; then
+		if [ -d /Zanthus/Zeus/path_comum/Descanso ]; then
+			rsync -ah /Zanthus/Zeus/path_comum/Descanso /Zanthus/Zeus/backup_"$data"/
+			cp -rfv "/Zanthus/Zeus/pdvJava/pdvGUI/guiConfigProj/imagens/$image2" /Zanthus/Zeus/path_comum/Descanso/imagens/
+		fi
+	fi
 fi
-
-
-
